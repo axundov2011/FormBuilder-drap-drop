2c982046d2fae7eff530fa3b5fd9c964f62033ac
aykhan.akhundov <aykhan.akhundov@a2z.az>
2023-12-23 23:00:00 +0400

    build

--- src/components/useFormBuilder.tsx
+++ src/components/useFormBuilder.tsx
@@ -1,50 +1,50 @@
 import { useState, useCallback } from 'react';
 
 export interface Element {
-  extension: string;
-  settings: {
-    [key: string]: any;
-  };
+    extension: string;
+    settings: {
+        [key: string]: any;
+    };
 }
 
 export interface Extension {
-  slug: string;
-  title: string;
-  description: string;
-  icon: React.ReactNode;
-  settings: {
-    [key: string]: any;
-  };
-  renderSettings: (settings: any) => React.ReactNode;
-  render: (settings: any) => React.ReactNode;
+    slug: string;
+    title: string;
+    description: string;
+    icon: React.ReactNode;
+    settings: {
+        [key: string]: any;
+    };
+    renderSettings: (settings: any) => React.ReactNode;
+    render: (settings: any) => React.ReactNode;
 }
 
 interface FormBuilder {
-  content: Element[];
-  addElement: (element: Element) => void;
-  removeElement: (index: number) => void;
+    content: Element[];
+    addElement: (element: Element) => void;
+    removeElement: (index: number) => void;
 }
 
 const useFormBuilder = (): FormBuilder => {
-  const [content, setContent] = useState<Element[]>([]);
+    const [content, setContent] = useState<Element[]>([]);
 
-  const addElement = useCallback((element: Element) => {
-    setContent((prevContent) => [...prevContent, element]);
-  }, []);
+    const addElement = useCallback((element: Element) => {
+        setContent((prevContent) => [...prevContent, element]);
+    }, []);
 
-  const removeElement = useCallback((index: number) => {
-    setContent((prevContent) => {
-      const newContent = [...prevContent];
-      newContent.splice(index, 1);
-      return newContent;
-    });
-  }, []);
+    const removeElement = useCallback((index: number) => {
+        setContent((prevContent) => {
+            const newContent = [...prevContent];
+            newContent.splice(index, 1);
+            return newContent;
+        });
+    }, []);
 
-  return {
-    content,
-    addElement,
-    removeElement,
-  };
+    return {
+        content,
+        addElement,
+        removeElement,
+    };
 };
 
 export default useFormBuilder;
